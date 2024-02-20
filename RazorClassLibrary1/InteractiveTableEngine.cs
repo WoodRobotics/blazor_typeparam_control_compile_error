d06183e2c5f76abccbca899c474831e2a2cf2500
Jason Wood <jason.wood@WDpartners.com>
2024-02-20 18:51:44 -0500

    Updated

--- RazorClassLibrary1/InteractiveTableEngine.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RazorClassLibrary1
-{
-    public class InteractiveTableEngine<T>
-    {
-        public List<InteractiveColumn<T>> Columns { get; private set; } = [];
-    }
-}
---
+++ RazorClassLibrary1/TestClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary1
+{
+    internal class TestClass
+    {
+
+        public TestControl PleaseWork { get; set; }
+
+    }
+}
